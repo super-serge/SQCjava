bd98eb28cff6c22f77e0a712cb3f11047d1bbd42
super-serge <a2032777@correo.uia.mx>
2017-02-26 19:59:34 -0600

    P5_03

--- SQCPracticas/p05/P5_02.java
+++ SQCPracticas/p05/P5_02.java
@@ -1,79 +1,79 @@
-
 import java.io.*;
 
 public class P5_02 {
 
     public static void main(String[] args) throws IOException {
 
-      int x, y, z;
-      float nz;
+        int x, y, z;
+        float nz;
 
-      BufferedReader stdin = new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader stdin = new BufferedReader(new InputStreamReader(System.in));
 
-      InputStreamReader serge = new InputStreamReader(System.in);
-      BufferedReader mme = new BufferedReader(serge);
+        InputStreamReader serge = new InputStreamReader(System.in);
+        BufferedReader mme = new BufferedReader(serge);
 
-      System.out.println("Hello\n");
-      //  String lineaTeclado = mme.readLine();
+        System.out.println("Hello\n");
+        //  String lineaTeclado = mme.readLine();
 
-      do {
-          System.out.println("Arreglo => Escribe un el tamaño ->\n");
-          int opE = Integer.parseInt(stdin.readLine());
-          x = opE % 2;
-          y = opE / 1;
-          int[] mDIO = new int[opE];
+        do {
+            System.out.println("Arreglo => Escribe un el tamaño ->");
+            int opE = Integer.parseInt(stdin.readLine());
+            x = opE % 2;
+            y = opE / 1;
+            int[] mDIO = new int[opE];
 
-          if (opE > 300 && opE < 0) {
-              System.out.println("Debe ser un Numero entre 0 - 300");
+            if (opE > 300 && opE < 0) {
+                System.out.println("Debe ser un Numero entre 0 - 300");
 
-          } else {
-              System.out.println("Correcto, tu arreglo sera de: " + opE + " valores \n");
+            } else {
+                System.out.println("Correcto, tu arreglo sera de: " + opE + " valores \n");
 
-          }
+            }
 
-      } while (x < 0 && x > 300);
-      double[] eDIO = new double[y];
-      double[] ar300 = new double[y];
+        } while (x < 0 && x > 300);
+        double[] eDIO = new double[y];
+        double[] ar300 = new double[y];
 
-      for (int j = 0; j < y; ++j) {
+        for (int j = 0; j < y; ++j) {
 
-          double valor;
-          int valor2;
-          valor = Math.random() * 300 + 1;
-          valor2 = (int) Math.random() * 300 + 1;
-          eDIO[j] = valor;
-      }
+            double valor;
+            int valor2;
+            valor = Math.random() * 300 + 1;
+            valor2 = (int) Math.random() * 300 + 1;
+            eDIO[j] = valor;
+        }
 
-      System.out.println("Que numero quieres\n");
+        System.out.println("\n Numero con que quieres que finalice -> ");
 
-      String lineaTeclado2 = mme.readLine();
-      int valorEN = Integer.parseInt(lineaTeclado2);
+        String lineaTeclado2 = mme.readLine();
+        int valorEN = Integer.parseInt(lineaTeclado2);
 
-      int n = 0;
-      int zAcum = 0;
-      int[] eFINAL = new int[zAcum];
-      for (int i = 0; i < y; ++i) {
+        int n = 0;
+        int zAcum = 0;
+        int[] eFINAL = new int[zAcum];
+        System.out.println("\nArreglo de ( " + y + " ) Numeros\n");
+        for (int i = 0; i < y; ++i) {
 
-          System.out.println(((int) eDIO[i]) + " -> " + (int) eDIO[i] % 10);
-          n = (int) eDIO[i] % 10;
+            System.out.println(((int) eDIO[i]) + " -> " + (int) eDIO[i] % 10);
+            n = (int) eDIO[i] % 10;
 
-          if (n == valorEN) {
-              zAcum = zAcum + 1;
-          }
+            if (n == valorEN) {
+                zAcum = zAcum + 1;
+            }
 
-      }
+        }
 
-      System.out.println("\n-----");
+        System.out.println("\n-----");
+        System.out.println("Los Numero que finalizan con [ " + valorEN + " ] son [ " + zAcum + " ] :");
 
-      int m = 0;
-      for (int i = 0; i < y; i++) {
-          if ((int) eDIO[i] % 10 == valorEN) {
+        int m = 0;
+        for (int i = 0; i < y; i++) {
+            if ((int) eDIO[i] % 10 == valorEN) {
 
-              System.out.println((int) eDIO[i]);
-          }
+                System.out.println((int) eDIO[i]);
+            }
 
-      }
+        }
 
-  }
-
+    }
 }
---
+++ SQCPracticas/p05/P5_03.java
@@ -0,0 +1,80 @@
+import java.io.*;
+public class P5_03{
+  static final int xMod=3;
+  public static void main(String[]args)throws IOException{
+
+    int x,y,z;
+    float nz;
+  BufferedReader stdin=new BufferedReader(new InputStreamReader(System.in));
+
+  InputStreamReader serge=new InputStreamReader(System.in);
+  BufferedReader mme =new BufferedReader(serge);
+
+  System.out.println("Hello");
+  System.out.println("Escribe El Nombre de 10 Alumnos y su Calificacion correspondiente: ");
+
+  int[]eDIO =new int[xMod];
+  double[]dbeDIO =new double[eDIO.length];
+  String[]eNAME =new String[xMod];
+
+String nameAL;
+double valor;
+String lineaTeclado;
+
+  for (int i=0,j=1;i<xMod ;++i,++j ) {
+    System.out.println("\nFull Name: <" + j + ">");
+
+    nameAL=stdin.readLine();
+
+    do{
+      System.out.println("Calificacion (valores del 0-10) : <" + j + ">");
+      lineaTeclado = mme.readLine();
+      valor = Double.parseDouble(lineaTeclado);
+      if(valor<0||valor>10){
+        System.out.println("Calificacion no Valida, trt again!");
+
+      }
+    }while(valor<0||valor>10);
+
+    dbeDIO[i]=valor;
+    eNAME[i]=nameAL;
+  }
+
+  String[]alFIN =new String[xMod];
+  String[]alFIN2 =new String[xMod];
+  //
+  //();
+  for (int i=0;i<xMod ;++i ) {
+    //String teampREG=eNAME[i].concat(", Calif. ["+String.valueOf(dbeDIO[i]));
+
+//    System.out.println(dbeDIO[i]+eNAME[i]);
+    if(dbeDIO[i]>=0&&dbeDIO[i]<5){
+
+
+    alFIN2[i]=eNAME[i].concat(", Calif. ["+String.valueOf(dbeDIO[i]).concat("], Reprobado."));
+
+}else if(dbeDIO[i]>=5&&dbeDIO[i]<7){
+  alFIN2[i]=eNAME[i].concat(", Calif. ["+String.valueOf(dbeDIO[i]).concat("], Bien."));
+
+}else if(dbeDIO[i]>=7&&dbeDIO[i]<9){
+  alFIN2[i]=eNAME[i].concat(", Calif. ["+String.valueOf(dbeDIO[i]).concat("], Notable."));
+
+}else if(dbeDIO[i]>=9&&dbeDIO[i]<=10){
+  alFIN2[i]=eNAME[i].concat(", Calif. ["+String.valueOf(dbeDIO[i]).concat("], Sobresaliente!!."));
+  //System.out.println(alFIN2[i]);
+}
+
+System.out.println("\nlas Calificaciones son: \n");
+
+  }
+  for (int k=0;k<xMod ;++k ) {
+      System.out.println(alFIN2[k]);
+    //  System.out.println(alFIN[i]);
+
+  }
+  System.out.println("\n ----------------------- \n");
+
+
+  }
+
+}
